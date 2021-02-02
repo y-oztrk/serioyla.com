a73c1b6e59e27a3632c42d03ec8ab915c18b7c9b
Furkan Kalyoncu <furkankalyoncu2000@hotmail.com>
2021-02-02 23:25:06 +0300

    Responsive sorununu JS ile düzelttiğim çözümü CSS ile düzelttim. Yeni seçenek buttonuna tıklayınca seçenek artık yeni buttonunun üstünde doğuyor. Ayrıca harika bir cümle kurdum az önce...

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-function isMobile() {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-        return true
-    } else {
-        return false
-    }
-}
-
 var counter = 3
 function addOption() {
     if (counter == null) {
@@ -16,7 +8,7 @@ function addOption() {
         return false
     }
     var newOptionDiv = $(document.createElement('div'))
-        .attr("id", 'newOption' + counter)
+        .attr("id", 'div-option-' + counter)
 
     $(newOptionDiv).after().html('<div class="input-group mb-3"><input type="text" class="form-control" placeholder="Anket seçeneği..." name="options-option-' + counter + '"><button class="btn btn-outline-secondary" type="button" onclick="delOption(' + counter + ')"><img src="images/trash.svg" alt="trash"></button></div>')
 
@@ -25,8 +17,8 @@ function addOption() {
 }
 
 function delOption(id) {
-    if (counter == 1) {
-        alert("No more textbox to remove");
+    if (counter == 3) {
+        alert("minimum 2 seçenek olmalı");
         return false;
     }
     counter--;
