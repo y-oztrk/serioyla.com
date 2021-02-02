6bb5807ab6780f9bf7b52c32858a62b4551fa268
Furkan Kalyoncu <furkankalyoncu2000@hotmail.com>
2021-02-02 21:09:59 +0300

    Input çoğaltma fonksiyonu eklendi

--- js/script.js
+++ js/script.js
@@ -1,47 +1,26 @@
 function isMobile() {
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         return true
-       } else {
-           return false
-       }
-}
-
-$(document).ready(function () {
-    for (i = 1; i <= 10; i++) {
-        $("#input_div").append('<input  id="count' + i.toString() + '" name="order" type="number" min="0" max="30" size="10" range="1" value="1">');
+    } else {
+        return false
     }
-});
-
-
-function myFunction() {
-    /* Get the text field */
-    var copyText = document.getElementById("myInput");
-
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-
-    /* Alert the copied text */
-    alert("Anket Linki Kopyalandı: " + copyText.value);
 }
-$(function () {
-    var scntDiv = $('#p_scents');
-    var i = $('#p_scents p').size() + 1;
 
-    $('#addScnt').live('click', function () {
-        $('<p><label for="p_scnts"><input type="text" id="p_scnt" size="20" name="p_scnt_' + i + '" value="" placeholder="Input Value" /></label> <a href="#" id="remScnt">Remove</a></p>').appendTo(scntDiv);
-        i++;
-        return false;
-    });
+var counter = 3
+function addOption() {
+    if (counter == null) {
+        counter = 1
+    }
+    if (counter > 10) {
+        alert("maksimum")
+        return false
+    }
+    var newOptionDiv = $(document.createElement('div'))
+        .attr("id", 'newOption' + counter)
 
-    $('#remScnt').live('click', function () {
-        if (i > 2) {
-            $(this).parents('p').remove();
-            i--;
-        }
-        return false;
-    });
-});
+    $(newOptionDiv).after().html('<div class="input-group mb-3"><input type="text" class="form-control" placeholder="Anket seçeneği..." name="options-option-' + counter + '"><button class="btn btn-outline-secondary" type="button"><img src="images/delete-24px.svg" alt="trash"></button></div>')
+
+    newOptionDiv.appendTo("#options-col")
+    counter++
+}
+
