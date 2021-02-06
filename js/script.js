b87ec7619955fd7e0b392ccaa3b7afa5123b37c7
DESKTOP-U2N87OI\Yigitcan <yigitcanozturk@ogr.bandirma.edu.tr>
2021-02-06 20:55:39 +0300

    oylama sayfa tasarımı güncellemesi
    sayfa tasarımı güncellemesi

--- js/script.js
+++ js/script.js
@@ -81,6 +81,14 @@ $("#create-poll").click(function () {
 
 
 
-
+function kopyalama(element){
+    var alan = element.parentElement.querySelector('a').innerHTML;
+    var textAlani = document.createElement('TEXTAREA');
+    textAlani.value = alan;
+    document.body.appendChild(textAlani);
+    textAlani.select();
+    document.execCommand('copy');
+    textAlani.style.display = 'none';
+  }
 
 
